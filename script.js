16aba061e996bd2a71aeae202321fda67f38c7d1
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-10-30 00:23:17 +0200

    Update appearance
    Also add buttons for future use
    
    Add swich to Fahrenheit button
    
    Add buttons to swap between 'today', 'now' and 'this week' view

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 const WEATHER_API_KEY = "e6eda6ffd30e45aebef74228232910 ";
 const WEATHER_BASE_URL = "https://api.weatherapi.com/v1";
 const content = document.querySelector(".content");
+const timeButtons = document.querySelector(".timeButtons");
 
-getWeather();
+getWeatherNow();
 
 async function getCoordinates() {
   return new Promise((resolve, reject) => {
@@ -21,7 +22,7 @@ async function getCoordinates() {
   });
 }
 
-async function getWeather() {
+async function getWeatherNow() {
   const formatter = new Intl.NumberFormat("en-us", {
     minimumIntegerDigits: 2,
     useGrouping: false,
@@ -30,6 +31,7 @@ async function getWeather() {
   const url = `${WEATHER_BASE_URL}/current.json?key=${WEATHER_API_KEY}&q=${lat},${long}`;
   const request = await fetch(url);
   const response = await request.json();
+
   const city = response.location.name;
   const status = response.current.condition.text;
   const temperature = response.current.temp_c;
@@ -40,9 +42,20 @@ async function getWeather() {
   )}`;
   const humid = response.current.humidity;
   const wind = response.current.wind_kph;
+  const windDir = response.current.wind_dir;
   const icon = response.current.condition.icon;
   console.log(response);
-  loadPageHTML(city, status, temperature, date, time, humid, wind, icon);
+  loadPageHTML(
+    city,
+    status,
+    temperature,
+    date,
+    time,
+    humid,
+    wind,
+    windDir,
+    icon
+  );
 }
 
 function loadPageHTML(
@@ -53,22 +66,34 @@ function loadPageHTML(
   time,
   humid,
   wind,
+  windDir,
   icon
 ) {
   const markup = `
     <div class="card">
-    <img src="images/cloudy.jpg" alt="" id="backgroundImage" />
-    <header class="city">${city}<span></span></header>
-    <div class="date">${date} <span>${time}</span></div>
-    <p class="status">${status}<span> <img src=${icon} alt="icon" id="weatherIcon"></span></p>
-    <p class="temp">Temperature: <span>${temperature}&deg;C</span></p>
-    <p class="humid">Humidity: <span>${humid}&#37;</span></p>
-    <p class="wind">
-      Wind:
-      <span>${wind} <span id="wind">km/h</span></span>
-    </p>
+      <header class="city">${city}<img src="${icon}"></header>
+      <div class="date">${date} <span>${time}</span></div>
+      <p class="status">Status: <span>${status}</span></p>
+      <p class="temp">Temperature: <span>${temperature}&deg;C</span></p>
+      <p class="humid">Humidity: <span>${humid}&#37;</span></p>
+      <p class="wind">
+        Wind:
+        <span>${wind} <span id="wind">km/h</span></span>
+      </p>
+      <p class="windDir">Wind direction: <span>${windDir}</span></p>
     </div>
   `;
 
   content.insertAdjacentHTML("beforeend", markup);
 }
+
+timeButtons.addEventListener("click", (e) => {
+  const btn = e.target.closest("button");
+  if (!btn) return;
+
+  document
+    .querySelectorAll(".timeButtons button")
+    .forEach((button) => button.classList.remove("activeTimeButton"));
+
+  btn.classList.add("activeTimeButton");
+});
